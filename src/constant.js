380a7ea0d62ff9f24683e5905e4125089e09a545
Priyanshuprinci <princi.it.js@gmail.com>
2024-05-07 21:15:36 +0530

    changing url

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
 // export const SERVER_ACCESS_URL="http://localhost:9000"
-export const SERVER_ACCESS_URL="https://ecommerce-backend-kl2u.onrender.com"
+export const SERVER_ACCESS_URL="https://ecommerce-backend-2r67.onrender.com"
